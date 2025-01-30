acf56d4db408356ebdae694ac67d74fb19a09af3
Cauã Israel <caua.israel02@gmail.com>
2025-01-30 10:13:04 -0300

    correção da função adicionar

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 //Função Lista de amigos 
-let amigos = [];
+let arrayAmigos = [];
+const resultadoSorteio = document.getElementById("amigoAleatorio");
 
 //Adicionar amigos
 function adicionarAmigo() {
-    const amigoInput = document.getElementById('nomeAmigo');
-    const nomeAmigo = amigoInput.value.trim();
-
-    if (nomeAmigo === ' ') {
+    const amigoInput = document.getElementById("amigo").value.trim();
+    if (amigoInput === "") {
         alert("Por favor, insira um nome.");
+        return
     } else {
-        amigos.push(nomeAmigo);        
+        arrayAmigos.push(amigoInput);
+        console.log(arrayAmigos);
+        const lista = document.getElementById("listaAmigos");
+        lista.innerHTML = `$(arrayAmigos)`  
     }
 }
 
 //Função para sortear amigos
  function sortearAmigo() {
-    const botaoResultado = document.getElementById('amigoAleatorio');
-
-    //verificar se tem amigo disponivel
-    if (amigos.length === 0) {
+     //verificar se tem amigo disponivel
+    if (arrayAmigos.length === 0) {
         botaoResultado.innerHTML = 'Nenhum amigo disponivel para sortear.';
         return
     }
+    const botaoResultado = document.getElementById('amigoAleatorio');
+
+   
  }
 
 // Gera um índice aleatório
